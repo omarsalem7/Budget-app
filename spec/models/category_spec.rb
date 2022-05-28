04c0a4be6ccacb1a864b4cbf9843d95877dd5870
omarsalem7 <omarsalem0721@gmail.com>
2022-05-29 01:37:50 +0200

    add spec for category model

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @user = User.create(name: 'Omar', email: 'omarsalem123@gmail.com', password: '123455')
+    expect(@user).to be_valid
+    @category = Category.create(name: 'hello', icon:"icon",
+                             user_id: @user.id)
+  end
+
+  it 'spec for create a category' do
+    expect(@category).to be_valid
+    expect(@category.name).to eq 'hello'
+    expect(@category.icon).to eq 'icon'
+  end
 end
