a36e7a72970981b29f3ed0e02cd3e56248c33819
omarsalem7 <omarsalem0721@gmail.com>
2022-05-29 01:20:29 +0200

    fix linetrs errors

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,7 +1,7 @@
 class Expense < ApplicationRecord
-    belongs_to :user
-    has_and_belongs_to_many :categories, dependent: :destroy
+  belongs_to :user
+  has_and_belongs_to_many :categories, dependent: :destroy
 
-    validates :name, presence: true
-    validates :amount, presence: true
+  validates :name, presence: true
+  validates :amount, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         has_many :categories, dependent: :destroy
-         has_many :expenses, dependent: :destroy
-       
-         validates :name, presence: true
+  has_many :categories, dependent: :destroy
+  has_many :expenses, dependent: :destroy
+
+  validates :name, presence: true
 end
